6c83134c42980ed8292391f20d5bce4036a7334f
Camilo Andrés Rojas Hernández <cm.andres022@hotmail.com>
2023-03-08 21:30:54 -0500

    Update
    Movement:
    - Physics using velocity vector
    - Input using Unity Input System
    Scenarios
    - Creation of colliders

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,19 +3,53 @@ using UnityEngine;
 
 namespace Player
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour
     {
         // Variables for Dungeon player game
-        public float speed = 5f;
+        [Header("Player Customization")]
         public string playerName = "Player";
+        
+        [Space]
+        [Header("Stats")]
+        public float speed = 5f;
         public int playerHealth = 100;
         public bool isAlive = true;
         public float playerDamage = 10f;
         
+        #region Components
+        private Rigidbody2D _rigidbody2D;
+        #endregion
+        
+        #region Variables for Player Movement
+        
+        private int _horizontalInput;
+        private int _verticalInput;
+
+        #endregion
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
         private void Start()
         {
-            print("Player name: " + playerName);
+            
         }
-        
+
+        private void Update()
+        {
+            // Set horizontal and vertical input
+            _horizontalInput = (int) Input.GetAxisRaw("Horizontal");
+            _verticalInput = (int) Input.GetAxisRaw("Vertical");
+            
+            // Diection
+            Vector2 direction = new Vector2(_horizontalInput, _verticalInput);
+
+            // Move player using physics and velocity
+            _rigidbody2D.velocity = direction * speed;
+        }
+
     }
 }
